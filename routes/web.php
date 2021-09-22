784170106bb6418178f4cbb9d736e608e9abb6be
L1m1tz <andreew.mk@gmail.com>
2021-09-22 14:43:35 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Display All Tasks
+
+Route::get('/', function () {
+    return view('tasks');
+});
+
+// 
+// Add A New Task
+// 
+Route::post('/task', function (Request $request) {
+    //
+});
+
+
+//Delete An Existing Task
+
+Route::delete('/task/{id}', function ($id) {
+    //
+});
