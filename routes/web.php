0c49c3aa55f3b603f75d973b4bf744e4763b04a0
L1m1tz <andreew.mk@gmail.com>
2021-09-22 15:20:00 +0200

    add post validation

--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 //Display All Tasks
-
 Route::get('/', function () {
     return view('tasks');
 });
 
-// 
-// Add A New Task
-// 
+
+// Add A New Task 
 Route::post('/task', function (Request $request) {
     //
 });
 
 
 //Delete An Existing Task
-
 Route::delete('/task/{id}', function ($id) {
     //
+});
+
+//Validation for name characters(should be less than 255)
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // Create The Task...
 });
