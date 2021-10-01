8ee2abbff092502dddeb728fdd8c9f63591905b0
L1m1tz <andreew.mk@gmail.com>
2021-10-01 11:07:11 +0200

    Fixed the error: "You may need an appropriate loader to handle this file type"

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,5 @@
+
+
 const mix = require('laravel-mix');
 
 /*
@@ -11,7 +13,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').vue()
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+    
