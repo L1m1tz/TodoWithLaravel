00f4aac959c27117a75dd471c5a09b918b2601d9
L1m1tz <andreew.mk@gmail.com>
2021-10-05 04:28:17 +0200

    added methods, & styles

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,12 +1,58 @@
 <template>
     <div>
-        Hello
+        <div class="todoListContainer">
+            <div class-="heading">
+                <h2 id="title"> Todo List</h2>
+                    <add-item-form v-on:reloadlist="getList()"/>
+              </div>
+              <list-view :items="items" v-on:reloadlist="getList()"/> 
+        </div>
+        
     </div>
 </template>
 
 <script>
-export default{
-   
- 
+import addItemForm from "./addItemForm";
+import listView from "./listView";
+
+export default {
+  components: {
+    addItemForm,
+    listView,
+  },
+  data: function(){
+    return{
+      items:[]
+    }
+  },
+  methods:{
+    getList(){
+      axios.get('api/items ')
+      .then( response =>{
+        this.items = response.data
+      })
+      .catch( error => {
+        console.log( error);
+      })
+    }
+  }, 
+  created() {
+    this.getList();
+  }
 }
 </script>
+
+<style scoped>
+.todoListContainer {
+  width: 350px;
+  margin: auto;
+}
+.heading {
+  background: rgba(24, 2, 2, 0.4);
+  padding: 10px;
+}
+
+#title {
+  text-align: center; 
+}
+</style>
